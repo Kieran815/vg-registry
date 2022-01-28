4349f8646ca9b105e595b9b9752835b8e827eb1d
Kieran Milligan <kieran.milligan@gmail.com>
2022-01-28 15:35:00 -0600

    got list creation working

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -17,36 +17,60 @@ export class GameListComponent implements OnInit {
   // for Material Expansion Panel (accordion)
   panelOpenState = false;
 
+  // var to receive body from modal list creation
+  createListBody: Object | undefined;
+
   constructor(private http: HttpClient, public dialog: MatDialog) { }
+
+  gameLists: any | undefined;
+  newListName: string | undefined;
+  newListDescription: string | undefined;
+  jsonResult: any;
 
   // method for angular list creation pop-up
   openDialog() {
-    this.dialog.open(ListCreationPopupComponent);
+    const dialogRef = this.dialog.open(ListCreationPopupComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      this.newListName = result.data.name;
+      this.newListDescription = result.data.description;
+      let nameExists = false;
+      if(this.newListName && this.newListDescription) {
+        for(let i = 0; i < this.gameLists.length; i++) {
+          if (this.gameLists[i].name === this.newListName) {
+            nameExists = true
+            alert(`List Not Saved. List name ${this.gameLists[i].name} already exists.`);
+          }
+        }
+        nameExists === false ? this.createList(this.newListName, this.newListDescription) : '';
+      } else {
+        alert("Missing Information. Please Review your Entry.")
+      }
+    })
   }
 
-  gameLists: any | undefined;
-
+  // ***** GET ALL LISTS *****
   getGameLists() {
     console.log("Connecting to IVGR DataBase...");
     this.http.get("http://localhost:9092/api/lists")
     .subscribe((response) => {
-      console.log(response);
       this.gameLists = response;
-      console.log(this.gameLists);
     })
   }
 
-  createList(listName: string, listDescription: string) {
-    console.log(`Creating List ${listName}`);
-
-    let body: any = {
-      name: listName,
-      description: listDescription
-    }
+  // ***** CREATE NEW LIST *****
+  createList(newName: string, newDescription: string) {
     this.http
-      .post("http://localhost:9092/api/lists", JSON.parse(body));
+      .post("http://localhost:9092/api/lists", {
+        name: newName,
+        description: newDescription
+      })
+      .toPromise()
+      .then((data: any) => {
+        console.log(data);
+        // console.log(JSON.stringify(data.json.name));
+        // this.jsonResult = JSON.stringify
+      })
       this.getGameLists();
-      // .pipe(this.handleError('Add List', ))
   }
 
   ngOnInit(): void {
--- src/app/list-creation-popup/list-creation-popup.component.ts
+++ src/app/list-creation-popup/list-creation-popup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-list-creation-popup',
@@ -11,7 +12,17 @@ export class ListCreationPopupComponent implements OnInit {
   listName: string = '';
   listDescription: string = '';
 
-  constructor(public dialog: MatDialog) { }
+  createNewList(newName: string, newDescription: string) {
+    this.listName = newName;
+    this.listDescription = newDescription;
+    let body = {
+      name: this.listName,
+      description: this.listDescription
+    }
+    this.dialogRef.close({ event: 'close', data: body });
+  }
+
+  constructor(public dialogRef: MatDialogRef<ListCreationPopupComponent>) { }
 
   ngOnInit(): void {
   }
